75bd91de0f867a09deead3cbb3971e31cdc97778
0xvikasrushi <0xvikas@gmail.com>
2023-02-19 14:08:55 +0530

    exports utils

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-import { encryptFolder } from "./utils/encrypt.js";
-import { decryptFolder } from "./utils/decrypt.js";
-import { uploadToIpfs } from "./utils/uploadipfs.js";
-import { downloadFile } from "./utils/retrieveipfs.js";
+import {
+  encryptFolder,
+  decryptFolder,
+  uploadToIpfs,
+  downloadFile,
+} from "./utils/index.js";
 
-async function uploadEncryptionIpfs(Web3Storagetoken, folderPath, password) {
+export async function uploadEncryptionIpfs(
+  Web3Storagetoken,
+  folderPath,
+  password
+) {
   await encryptFolder(folderPath, password);
   const cid = await uploadToIpfs(Web3Storagetoken, folderPath);
   return cid;
 }
 
-async function decryptFolderIpfs(Web3Storagetoken, cid, password) {
+export async function decryptFolderIpfs(Web3Storagetoken, cid, password) {
   await downloadFile(Web3Storagetoken, cid);
   const res = await decryptFolder(".", password);
   return res;
 }
-
+export {
+  encryptFolder,
+  decryptFolder,
+  uploadToIpfs,
+  downloadFile,
+} from "./utils/index.js";
---
+++ utils/index.js
@@ -0,0 +1,4 @@
+export { encryptFolder } from "./encrypt.js";
+export { decryptFolder } from "./decrypt.js";
+export { uploadToIpfs } from "./uploadipfs.js";
+export { downloadFile } from "./retrieveipfs.js";
